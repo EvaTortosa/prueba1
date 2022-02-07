2739b18f5e304af7fc85ce61a2afa400a87de518
EvaTortosa <48592760+EvaTortosa@users.noreply.github.com>
2022-02-07 20:03:11 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+let strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
+strip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+strip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+strip.setPixelColor(2, neopixel.colors(NeoPixelColors.Purple))
+strip.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
+strip.show()
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
